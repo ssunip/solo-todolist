fa0fe16b63ef81c53888a9ff3927bba9d73f08df
ssunip <ssunip99@gmail.com>
2022-09-30 16:34:02 +0900

    기초 ui 완성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createGlobalStyle } from "styled-components";
+import Template from "./components/Template";
+import TodoHeader from "./components/TodoHeader";
+import TodoList from "./components/TodoList";
+import TodoCreate from "./components/TodoCreate";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const GlobalStyle = createGlobalStyle`
+body {
+  background: #e9ecef;
 }
+`;
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Template>
+        <TodoHeader />
+        <TodoList />
+        <TodoCreate />
+      </Template>
+    </>
+  );
+};
 
 export default App;
