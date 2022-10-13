31bc37be70709933f0731cf5bc9a2ca4bbf30020
ssunip <ssunip99@gmail.com>
2022-10-14 00:37:43 +0900

    TodoCreate css 완료

--- src/components/TodoCreate.js
+++ src/components/TodoCreate.js
@@ -1,77 +1,90 @@
 import React, { useState } from "react";
 import styled, { css } from "styled-components";
-import { MdAdd } from "react-icons/md";
+import { MdAdd, MdAddCircle } from "react-icons/md";
+
+const Background = styled.div`
+  position: absolute;
+  z-index: 980;
+  top: 0;
+  left: 0;
+  overflow: hidden;
+  width: 375px;
+  height: 812px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #00000050;
+`;
 
 const CircleButton = styled.button`
-  background: #38d9a9;
+  background: #c3b274;
   &:hover {
-    background: #63e6be;
+    background: #c3b274;
   }
   &:active {
-    background: #20c997;
+    background: #c3b274;
   }
 
   z-index: 5;
   cursor: pointer;
-  width: 80px;
-  height: 80px;
-  display: block;
+  width: 60px;
+  height: 60px;
   align-items: center;
   justify-content: center;
-  font-size: 60px;
   position: absolute;
-  left: 50%;
-  bottom: 0px;
+  font-size: 60px;
+  right: -10px;
+  bottom: 50px;
   transform: translate(-50%, 50%);
-  color: white;
+  color: #414141;
   border-radius: 50%;
   border: none;
   outline: none;
   display: flex;
-  align-items: center;
-  justify-content: center;
 
   transition: 0.125s all ease-in;
   ${(props) =>
     props.open &&
     css`
-      background: #ff6b6b;
-      &:hover {
-        background: #ff8787;
-      }
-      &:active {
-        background: #fa5252;
-      }
       transform: translate(-50%, 50%) rotate(45deg);
     `}
 `;
 
 const InsertFormPositioner = styled.div`
-  width: 100%;
-  bottom: 0;
-  left: 0;
+  width: 80%;
   position: absolute;
 `;
 
 const InsertForm = styled.form`
-  background: #f8f9fa;
-  padding-left: 32px;
-  padding-top: 32px;
-  padding-right: 32px;
-  padding-bottom: 72px;
+  background: #414141;
+  width: 300px;
+  height: 200px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
 
-  border-bottom-left-radius: 16px;
-  border-bottom-right-radius: 16px;
-  border-top: 1px solid #e9ecef;
+  .add-btn {
+    margin-top: 30px;
+    font-size: 40px;
+    color: #c3b274;
+  }
+`;
+
+const Text = styled.div`
+  margin: 10px;
+  color: white;
 `;
 
 const Input = styled.input`
-  padding: 12px;
-  border-radius: 4px;
-  border: 1px solid #dee2e6;
-  width: 100%;
+  margin-top: 30px;
+  padding: 10px;
+  border: none;
+  border-bottom: 1px solid white;
+  width: 80%;
   outline: none;
+  color: white;
   font-size: 18px;
+  background: #414141;
   box-sizing: border-box;
 `;
 
@@ -82,16 +95,20 @@ const TodoCreate = () => {
 
   return (
     <>
-      {open && (
-        <InsertFormPositioner>
-          <InsertForm>
-            <Input autoFocus placeholder="할 일을 입력 후, Enter 를 누르세요" />
-          </InsertForm>
-        </InsertFormPositioner>
-      )}
-      <CircleButton onClick={onToggle} open={open}>
-        <MdAdd />
-      </CircleButton>
+      <Background>
+        {open && (
+          <InsertFormPositioner>
+            <InsertForm>
+              <Text>Add new thing!</Text>
+              <Input autoFocus placeholder="Enter" />
+              <MdAddCircle className="add-btn" />
+            </InsertForm>
+          </InsertFormPositioner>
+        )}
+        <CircleButton onClick={onToggle} open={open}>
+          <MdAdd />
+        </CircleButton>
+      </Background>
     </>
   );
 };
